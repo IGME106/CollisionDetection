ee5676c752174ff43e3bb6484bcc261203d03fdc
bxk8027 <bxk8027@IGM2025-29>
2018-02-16 10:23:26 -0500

    Start with the homework assignment.  General class creation.  Design still in progress.

---
+++ CollisionDetection/GameObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CollisionDetection
+{
+    class GameObjects : Game1
+    {
+        private enum GameState
+        {
+            Menu,
+            Game,
+            GameOver
+        }
+
+        private GameState currentGameState = GameState.Menu;
+
+
+    }
+}
